efbcfe2db1742fe8a95653233de46eab57822fd1
AZ404 <ztbzak1@rambler.ru>
2017-11-29 13:08:25 +0500

    done

--- async.js
+++ async.js
@@ -7,7 +7,53 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise((resolve) => {
+        const queue = jobs.slice();
+        const results = new Map();
+        let runningCount = 0;
+
+        function start(job) {
+            runningCount++;
+
+            Promise.race([
+                job()
+                    .catch(err => err),
+                delay(timeout)
+                    .then(() => new Error('Promise timeout'))
+            ])
+                .then(result => {
+                    results[job] = result;
+                    runningCount--;
+                    startNext();
+                    if (runningCount === 0) {
+                        onComplete();
+                    }
+                });
+        }
+
+        function startNext() {
+            const job = queue.shift();
+            if (!job) {
+                return;
+            }
+            start(job);
+        }
+
+        function onComplete() {
+            resolve(jobs.map(job => results[job]));
+        }
+
+        for (let i = 0; i < parallelNum; i++) {
+            startNext();
+        }
+    });
+}
+
+function delay(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout);
+    });
 }
